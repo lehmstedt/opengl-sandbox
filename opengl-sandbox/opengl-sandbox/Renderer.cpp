f812a40272d3756b6f12c0c40d7b61aada3cabb6
Sébastien Lehmstedt <lehmstedt-sebastien@hotmail.fr>
2018-10-07 12:49:41 +0200

    Utilise une matrice de rotation

--- opengl-sandbox/opengl-sandbox/Renderer.cpp
+++ opengl-sandbox/opengl-sandbox/Renderer.cpp
@@ -60,8 +60,8 @@ void Renderer::Render()
 
 void Renderer::SetWorldMatrix()
 {
-	m_world.m[0][0] = 1.0f; m_world.m[0][1] = 0.0f; m_world.m[0][2] = 0.0f; m_world.m[0][3] = sinf(m_scale);
-	m_world.m[1][0] = 0.0f; m_world.m[1][1] = 1.0f; m_world.m[1][2] = 0.0f; m_world.m[1][3] = 0.0f;
-	m_world.m[2][0] = 0.0f; m_world.m[2][1] = 0.0f; m_world.m[2][2] = 1.0f; m_world.m[2][3] = 0.0f;
-	m_world.m[3][0] = 0.0f; m_world.m[3][1] = 0.0f; m_world.m[3][2] = 0.0f; m_world.m[3][3] = 1.0f;
+	m_world.m[0][0] = cosf(m_scale); m_world.m[0][1] = -sinf(m_scale); m_world.m[0][2] = 0.0f; m_world.m[0][3] = 0.0f;
+	m_world.m[1][0] = sinf(m_scale); m_world.m[1][1] = cosf(m_scale);  m_world.m[1][2] = 0.0f; m_world.m[1][3] = 0.0f;
+	m_world.m[2][0] = 0.0f;        m_world.m[2][1] = 0.0f;         m_world.m[2][2] = 1.0f; m_world.m[2][3] = 0.0f;
+	m_world.m[3][0] = 0.0f;        m_world.m[3][1] = 0.0f;         m_world.m[3][2] = 0.0f; m_world.m[3][3] = 1.0f;
 }
